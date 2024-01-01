dda890e282109a6a372b45da67deae38ad412263
Vasil Delchev <vasildelchev97@gmail.com>
2024-01-01 17:30:17 +0200

    commited exercise 3

---
+++ 01.StacksAndQueues-Exercise/03.MaximumAndMinimumElement/Program.cs
@@ -0,0 +1,33 @@
+﻿
+int numberOfCommands = int.Parse(Console.ReadLine());
+
+Stack<int> numbers = new Stack<int>();
+
+for (int i = 0; i < numberOfCommands; i++)
+{
+    string[] commandInfo = Console.ReadLine()
+        .Split();
+
+    string commandName = commandInfo[0];
+
+    if (commandName == "1")
+    {
+        int numberToPush = int.Parse(commandInfo[1]);
+
+        numbers.Push(numberToPush);
+    }
+    else if (commandName == "2" && numbers.Any())
+    {
+        numbers.Pop();
+    }
+    else if (commandName == "3" && numbers.Any())
+    {
+        Console.WriteLine(numbers.Max());
+    }
+    else if (commandName == "4" && numbers.Any())
+    {
+        Console.WriteLine(numbers.Min());
+    }
+}
+
+Console.WriteLine(string.Join(", ", numbers));
