b8a19a42ceeb82824b9761391ef0e341ac8f4589
Vasil Delchev <vasildelchev97@gmail.com>
2024-01-03 21:45:31 +0200

    Commit and Push exercises 8 and 9

--- 01.StacksAndQueues-Exercise/08.BalancedParenthesis/Program.cs
+++ 01.StacksAndQueues-Exercise/08.BalancedParenthesis/Program.cs
@@ -1,1 +1,36 @@
 ﻿
+string input = Console.ReadLine();
+
+Stack<char> brackets = new Stack<char>();
+
+for (int i = 0; i < input.Length; i++)
+{
+    if (input[i] == '('
+        || input[i] == '['
+        || input[i] == '{')
+    {
+        brackets.Push(input[i]);
+    }
+    else if ((input[i] == ')' && brackets.Count == 0)
+        || (input[i] == '}' && brackets.Count == 0)
+        || (input[i] == ']' && brackets.Count == 0))
+    {
+        brackets.Push(input[i]);
+        break;
+    }
+    else if ((input[i] == ')' && brackets.Peek() == '(')
+        || (input[i] == ']' && brackets.Peek() == '[')
+        || (input[i] == '}' && brackets.Peek() == '{'))
+    {
+        brackets.Pop();
+    }
+}
+
+if (brackets.Count == 0)
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.WriteLine("NO");
+}
---
+++ 01.StacksAndQueues-Exercise/09.SimpleTextEditor/Program.cs
@@ -0,0 +1,48 @@
+﻿
+int numberOfCommands = int.Parse(Console.ReadLine());
+
+string text = string.Empty;
+
+Stack<string> stack = new Stack<string>();
+
+stack.Push(text);
+
+for (int i = 0; i < numberOfCommands; i++)
+{
+    string[] commandInfo = Console.ReadLine()
+        .Split();
+
+    string commandName = commandInfo[0];
+
+    if (commandName == "1")
+    {
+        string addString = commandInfo[1];
+
+        text += addString;
+        stack.Push(text);
+    }
+    else if (commandName == "2")
+    {
+        int count = int.Parse(commandInfo[1]);
+
+        text = text.Remove(text.Length - count, count);
+
+        stack.Push(text);
+    }
+    else if (commandName == "3")
+    {
+        int index = int.Parse(commandInfo[1]);
+
+        string currentText = stack.Peek();
+
+        char charToPrint = currentText[index - 1];
+
+        Console.WriteLine(charToPrint);
+    }
+    else if (commandName == "4")
+    {
+        stack.Pop();
+
+        text = stack.Peek();
+    }
+}
