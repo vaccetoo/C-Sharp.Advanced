addee2f9588b1148e782520c92dd0907133b096a
Vasil Delchev <vasildelchev97@gmail.com>
2024-01-04 23:20:49 +0200

    Added exercises 10 and 11

---
+++ 01.StacksAndQueues-Exercise/10.Crossroads/Program.cs
@@ -0,0 +1,64 @@
+﻿
+int greenLightDuration = int.Parse(Console.ReadLine());
+int freeWindowDuration = int.Parse(Console.ReadLine());
+
+string command = string.Empty;
+
+Queue<string> cars = new Queue<string>();
+
+int passedCars = 0;
+
+int currentGreenDuration = greenLightDuration;
+
+bool isCrashHappend = false;
+
+while ((command = Console.ReadLine()) != "END")
+{
+    if (command != "green") // We recive car 
+    {
+        cars.Enqueue(command);
+        continue;
+    }
+
+    // The command "green" indicates the start of a green light cycle
+
+    currentGreenDuration = greenLightDuration;
+
+    while (cars.Any() && currentGreenDuration > 0)
+    {
+        string currentCar = cars.Peek();
+
+        if (currentGreenDuration - currentCar.Length >= 0)
+        {
+            currentGreenDuration -= currentCar.Length;
+            cars.Dequeue();
+            passedCars++;
+        }
+        else if (currentGreenDuration + freeWindowDuration - currentCar.Length >= 0)
+        {
+            currentGreenDuration = 0;
+            cars.Dequeue();
+            passedCars++;
+        }
+        else
+        {
+            char crashedIndex = currentCar[currentGreenDuration + freeWindowDuration];
+            Console.WriteLine("A crash happened!");
+            Console.WriteLine($"{currentCar} was hit at {crashedIndex}.");
+
+            isCrashHappend = true;
+            break;
+        }
+    }
+
+    if (isCrashHappend)
+    {
+        break;
+    }
+}
+
+if (command == "END")
+{
+    Console.WriteLine("Everyone is safe.");
+    Console.WriteLine($"{passedCars} total cars passed the crossroads.");
+}
---
+++ 01.StacksAndQueues-Exercise/11.KeyRevolver/Program.cs
@@ -0,0 +1,61 @@
+﻿
+int bulletPrice = int.Parse(Console.ReadLine());
+
+int gunBarrelSize  = int.Parse(Console.ReadLine());
+
+int[] bulletsValues = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+int[] locksValues = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+int intelligenceValue = int.Parse(Console.ReadLine()); // Also will be used for moneyEarned = intelligence - moneySpend
+
+Queue<int> locks = new Queue<int>(locksValues);
+
+Stack<int> bullets = new Stack<int>(bulletsValues);
+
+int currentBarrelSize = gunBarrelSize;
+
+int moneySpend = 0;
+
+while (bullets.Any() && locks.Any())
+{
+    int currentLockValue = locks.Peek();
+    int currentBulletValue = bullets.Pop();
+
+    moneySpend += bulletPrice;
+    currentBarrelSize--;
+
+    if (currentBulletValue <= currentLockValue)
+    {
+        Console.WriteLine("Bang!");
+        locks.Dequeue();
+    }
+    else
+    {
+        Console.WriteLine("Ping!");
+    }
+
+    if (currentBarrelSize == 0 && bullets.Any())
+    {
+        Console.WriteLine("Reloading!");
+        currentBarrelSize = gunBarrelSize;
+    }
+
+    if (!bullets.Any() && locks.Any())
+    {
+        Console.WriteLine($"Couldn't get through. Locks left: {locks.Count()}");
+    }
+}
+
+if (!locks.Any())
+{
+    intelligenceValue -= moneySpend;
+
+    Console.WriteLine($"{bullets.Count()} bullets left. Earned ${intelligenceValue}");
+}
