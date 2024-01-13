1fda7666bc7249f8b013c585db87df246d938302
Vasil Delchev <vasildelchev97@gmail.com>
2024-01-13 13:32:56 +0200

    Added more exercises

---
+++ 02.MultidimensionalArrays-Exercise/08.Bombs/Program.cs
@@ -0,0 +1,176 @@
+﻿
+using System.ComponentModel;
+
+int squareMatrixSize = int.Parse(Console.ReadLine());
+
+int rows = squareMatrixSize;
+int cols = squareMatrixSize;
+
+int[,] board = new int[rows, cols];
+
+for (int row = 0; row < rows; row++)
+{
+    int[] cellValues = Console.ReadLine()
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+        .Select(int.Parse)
+        .ToArray();
+
+    for (int col = 0; col < cols; col++)
+    {
+        board[row, col] = cellValues[col];
+    }
+}
+
+string[] bombs = Console.ReadLine()
+    .Split();
+
+foreach (var bomb in bombs)
+{
+    int[] currentBombIndexes = bomb
+        .Split(",", StringSplitOptions.RemoveEmptyEntries)
+        .Select(int.Parse)
+        .ToArray();
+
+    int currentBombRow = currentBombIndexes[0];
+    int currentBombCol = currentBombIndexes[1];
+
+    for (int row = 0; row < rows; row++)
+    {
+        for (int col = 0; col < cols; col++)
+        {
+            if (row == currentBombRow && col == currentBombCol) // There is a bomb
+            {
+                // TODO: The bomb has to explode ... by 1 square UP, DOWN, LEFT, RIGHT, BOTH DIAGONALS
+
+                int bombDamage = board[row, col];
+
+                if (bombDamage > 0) // The bomb can Explode only if it has value greater than 0
+                {
+                    Explode(currentBombRow, currentBombCol, board, bombDamage);
+                }
+            }
+        }
+    }
+}
+
+int aliveCells = 0;
+int sumCells = 0;
+
+for (int row = 0; row < rows; row++)
+{
+    for (int col = 0; col < cols; col++)
+    {
+        if (board[row, col] > 0)
+        {
+            aliveCells++;
+            sumCells += board[row, col];
+        }
+    }
+}
+
+Console.WriteLine($"Alive cells: {aliveCells}");
+
+Console.WriteLine($"Sum: {sumCells}");
+
+PrintBoard(board);
+
+void Explode(int currentBombRow, int currentBombCol, int[,] board, int damage)
+{
+    // 1 up
+    if (IsCellVallid(currentBombRow - 1, currentBombCol))
+    {
+        if (board[currentBombRow - 1, currentBombCol] > 0) // Is not dead
+        {
+            board[currentBombRow - 1, currentBombCol] -= damage;
+        }
+    }
+
+    // 1 up 1 left
+    if (IsCellVallid(currentBombRow - 1, currentBombCol - 1))
+    {
+        if (board[currentBombRow - 1, currentBombCol - 1] > 0) // Is not dead
+        {
+            board[currentBombRow - 1, currentBombCol - 1] -= damage;
+        }
+    }
+
+    // 1 up 1 right
+    if (IsCellVallid(currentBombRow - 1, currentBombCol + 1))
+    {
+        if (board[currentBombRow - 1, currentBombCol + 1] > 0) // Is not dead
+        {
+            board[currentBombRow - 1, currentBombCol + 1] -= damage;
+        }
+    }
+
+    // 1 left
+    if (IsCellVallid(currentBombRow, currentBombCol - 1))
+    {
+        if (board[currentBombRow, currentBombCol - 1] > 0) // Is not dead
+        {
+            board[currentBombRow, currentBombCol - 1] -= damage;
+        }
+    }
+
+    // 1 right
+    if (IsCellVallid(currentBombRow, currentBombCol + 1))
+    {
+        if (board[currentBombRow, currentBombCol + 1] > 0) // Is not dead
+        {
+            board[currentBombRow, currentBombCol + 1] -= damage;
+        }
+    }
+
+    // 1 down 
+    if (IsCellVallid(currentBombRow + 1, currentBombCol))
+    {
+        if (board[currentBombRow + 1, currentBombCol] > 0) // Is not dead
+        {
+            board[currentBombRow + 1, currentBombCol] -= damage;
+        }
+    }
+
+    // 1 down 1 left
+    if (IsCellVallid(currentBombRow + 1, currentBombCol - 1))
+    {
+        if (board[currentBombRow + 1, currentBombCol - 1] > 0) // Is not dead
+        {
+            board[currentBombRow + 1, currentBombCol - 1] -= damage;
+        }
+    }
+
+    // 1 down 1 right
+    if (IsCellVallid(currentBombRow + 1, currentBombCol + 1))
+    {
+        if (board[currentBombRow + 1, currentBombCol + 1] > 0) // Is not dead
+        {
+            board[currentBombRow + 1, currentBombCol + 1] -= damage;
+        }
+    }
+
+    board[currentBombRow, currentBombCol] = 0;
+}
+
+bool IsCellVallid(int currentRow, int currentCol)
+{
+    if (currentRow >= 0 && currentRow < rows &&
+        currentCol >= 0 && currentCol < cols)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+void PrintBoard(int[,] board)
+{
+    for (int row = 0; row < rows; row++)
+    {
+        for (int col = 0; col < cols; col++)
+        {
+            Console.Write(board[row, col] + " ");
+        }
+
+        Console.WriteLine();
+    }
+}
---
+++ 02.MultidimensionalArrays-Exercise/09.Miner/Program.cs
@@ -0,0 +1,155 @@
+﻿
+int fieldSizes = int.Parse(Console.ReadLine());
+
+int rows = fieldSizes;
+int cols = fieldSizes;
+
+char[,] field = new char[rows, cols];
+
+Queue<string> commands = new Queue<string>
+    (Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries));  
+
+for (int row = 0; row < rows; row++)
+{
+    char[] cellValues = Console.ReadLine()
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+        .Select(char.Parse)
+        .ToArray();
+
+    for (int col = 0; col < cols; col++)
+    {
+        field[row, col] = cellValues[col];
+    }
+}
+
+// Field positions:
+// * – a regular position on the field
+// e – the end of the route
+// c  - coal
+// s - the place where the miner starts
+
+int currentRow = 0;
+int currentCol = 0;
+
+int coalsCount = 0;
+
+bool isGameOver = false;
+
+for (int row = 0; row < rows; row++)
+{
+    for (int col = 0; col < cols; col++)
+    {
+        if (field[row, col] == 's')
+        {
+            currentRow = row;
+            currentCol = col;
+        }
+
+        if (field[row, col] == 'c')
+        {
+            coalsCount++;
+        }
+    }
+}
+
+int coalsFound = 0;
+
+while (commands.Any()) // TODO: Break if cell with 'e' is found or have collected all coals
+{
+    string currentCommand = commands.Dequeue();
+
+    if (currentCommand == "left")
+    {
+        if (IsCellVallid(currentRow, currentCol - 1))
+        {
+            currentCol = currentCol - 1;
+
+            CheckCurrentCell(currentRow, currentCol);
+
+            if (isGameOver)
+            {
+                break;
+            }
+        }
+    }
+    else if (currentCommand == "right")
+    {
+        if (IsCellVallid(currentRow, currentCol + 1))
+        {
+            currentCol = currentCol + 1;
+
+            CheckCurrentCell(currentRow, currentCol);
+
+            if (isGameOver)
+            {
+                break;
+            }
+        }
+    }
+    else if (currentCommand == "up")
+    {
+        if (IsCellVallid(currentRow - 1, currentCol))
+        {
+            currentRow = currentRow - 1;
+
+            CheckCurrentCell(currentRow, currentCol);
+
+            if (isGameOver)
+            {
+                break;
+            }
+        }
+    }
+    else if (currentCommand == "down")
+    {
+        if (IsCellVallid(currentRow + 1, currentCol))
+        {
+            currentRow = currentRow + 1;
+
+            CheckCurrentCell(currentRow, currentCol);
+
+            if (isGameOver)
+            {
+                break;
+            }
+        }
+    }
+
+    if (coalsFound == coalsCount)
+    {
+        isGameOver = true;
+        Console.WriteLine($"You collected all coals! ({currentRow}, {currentCol})");
+        break;
+    }
+}
+
+if (!commands.Any() && !isGameOver)
+{
+    Console.WriteLine($"{coalsCount - coalsFound} coals left. ({currentRow}, {currentCol})");
+}
+
+void CheckCurrentCell(int currentRow, int currentCol)
+{
+    if (field[currentRow, currentCol] == 'c')
+    {
+        coalsFound++;
+        field[currentRow, currentCol] = '*';
+    }
+    else if (field[currentRow, currentCol] == 'e')
+    {
+        isGameOver = true;
+        Console.WriteLine($"Game over! ({currentRow}, {currentCol})");
+    }
+}
+
+bool IsCellVallid(int row, int col)
+{
+    if (row >= 0 && row < field.GetLength(0) &&
+        col >= 0 && col < field.GetLength(1))
+    {
+        return true;
+    }
+
+    return false;
+}
