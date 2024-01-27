f64d8cd2305f81ba89d1ff8b1d87a99370e25f42
Vasil Delchev <vasildelchev97@gmail.com>
2024-01-27 22:05:53 +0200

    Added PokemonTrainers

---
+++ 06.DefiningClasses-Exercise/06.PokemonTrainer/StartUp.cs
@@ -0,0 +1,80 @@
+﻿
+namespace PokemonTrainer;
+
+class StartUp
+{
+    public static void Main()
+    {
+        string info = string.Empty;
+
+        List<Trainer> trainers = new List<Trainer>();
+
+        while ((info = Console.ReadLine()) != "Tournament")
+        {
+            string[] trainerPokemonInfo = info
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Pokemon pokemon = CreatePokemon(trainerPokemonInfo);
+
+            string trainerName = trainerPokemonInfo[0];
+
+            if (trainers.Any(n => n.Name == trainerName))
+            {
+                trainers.First(n => n.Name == trainerName).AddPokemon(pokemon);
+            }
+            else
+            {
+                Trainer trainer = CreateTrainer(trainerName, pokemon);
+                trainers.Add(trainer);
+            }
+        }
+
+        string command = string.Empty;
+
+        while ((command = Console.ReadLine()) != "End")
+        {
+            string currentElement = command;
+
+            foreach (Trainer trainer in trainers)
+            {
+                if (trainer.Pokemons.Any(n => n.Element == currentElement))
+                {
+                    trainer.NumberOfBadges += 1;
+                }
+                else
+                {
+                    trainer.Pokemons.ForEach(h => h.Health -= 10);
+                }
+
+                if (trainer.Pokemons.Any(h => h.Health <= 0))
+                {
+                    trainer.Pokemons.RemoveAll(h => h.Health <= 0);
+                }
+            }
+        }
+
+        foreach (Trainer trainer in trainers.OrderByDescending(n => n.NumberOfBadges))
+        {
+            Console.WriteLine($"{trainer.Name} {trainer.NumberOfBadges} {trainer.Pokemons.Count()}");
+        }
+    }
+
+    private static Trainer CreateTrainer(string trainerName, Pokemon pokemon)
+    {
+        Trainer trainer = new Trainer(trainerName);
+        trainer.AddPokemon(pokemon);
+
+        return trainer;
+    }
+
+    private static Pokemon CreatePokemon(string[] pokemonInfo)
+    {
+        string name = pokemonInfo[1];
+        string element = pokemonInfo[2];
+        int health = int.Parse(pokemonInfo[3]);
+
+        Pokemon pokemon = new Pokemon(name, element, health);
+
+        return pokemon;
+    }
+}
---
+++ 06.DefiningClasses-Exercise/06.PokemonTrainer/Trainer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonTrainer
+{
+    public class Trainer
+    {
+        private List<Pokemon> pokemons;
+
+        public Trainer(string name)
+        {
+            Name = name;
+            Pokemons = new List<Pokemon>();
+        }
+        public string Name { get; set; }
+
+        public int NumberOfBadges { get; set; }
+
+        public List<Pokemon> Pokemons { get { return pokemons; } set { pokemons = value; } }
+
+        public void AddPokemon(Pokemon pokemon)
+        {
+            Pokemons.Add(pokemon);
+        }
+    }
+}
