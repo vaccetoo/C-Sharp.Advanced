709fb4fe9e63a257779bad49efa1d3c4bb1622e9
Vasil Delchev <vasildelchev97@gmail.com>
2024-02-06 06:27:54 +0200

    LocalCommit

--- 09.ExamPreparation/MonsterExtermination/Program.cs
+++ 09.ExamPreparation/MonsterExtermination/Program.cs
@@ -1,1 +1,56 @@
 ﻿
+int[] monsterArmorsAmmounts = Console.ReadLine()
+    .Split(",", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+int[] soldiersAttacksAmmounts = Console.ReadLine()
+    .Split(",", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+Queue<int> monstersArmor =  new Queue<int>(monsterArmorsAmmounts);
+Stack<int> soldiersAttack =  new Stack<int>(soldiersAttacksAmmounts);
+
+int killedMonsters = 0;
+
+while (monstersArmor.Any() && soldiersAttack.Any())
+{
+    int currentAttack = soldiersAttack.Peek();
+    int currentArmor = monstersArmor.Peek();
+
+    if (currentAttack >= currentArmor)
+    {
+        killedMonsters++;
+
+        monstersArmor.Dequeue();
+
+        int newAttackValue = currentAttack - currentArmor;
+        soldiersAttack.Pop();
+
+        if (newAttackValue > 0)
+        {
+            soldiersAttack.Push(newAttackValue);
+        }
+    }
+    else
+    {
+        int monsterNewArmor = currentArmor - currentAttack;
+        monstersArmor.Dequeue();
+        monstersArmor.Enqueue(monsterNewArmor);
+
+        soldiersAttack.Pop();
+    }
+
+    if (!monstersArmor.Any())
+    {
+        Console.WriteLine($"All monsters have been killed!");
+    }
+}
+
+if (!soldiersAttack.Any())
+{
+    Console.WriteLine($"The soldier has been defeated.");
+}
+
+Console.WriteLine($"Total monsters killed: {killedMonsters}");
