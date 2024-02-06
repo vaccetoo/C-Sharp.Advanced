54df8673d4ddf5eb9308d7cf7dbc8043d2663743
Vasil Delchev <vasildelchev97@gmail.com>
2024-02-06 05:59:03 +0200

    Local commit

---
+++ 09.ExamPreparation/MailClient/Mail.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailClient
+{
+    public class Mail
+    {
+        public Mail(string sender, string receiver, string body)
+        {
+            Sender = sender;
+            Receiver = receiver;
+            Body = body;
+        }
+
+        public string Sender { get; set; }
+
+        public string Receiver { get; set; }
+
+        public string Body { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"From: {Sender} / To: {Receiver}");
+            sb.AppendLine($"Message: {Body}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ 09.ExamPreparation/MailClient/MailBox.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailClient
+{
+    public class MailBox
+    {
+        public MailBox(int capacity)
+        {
+            Capacity = capacity;
+            Inbox = new List<Mail>();
+            Archive = new List<Mail>();
+        }
+
+        public int Capacity { get; set; }
+
+        public List<Mail> Inbox { get; set; }
+
+        public List<Mail> Archive { get; set; }
+
+        public void IncomingMail(Mail mail)
+        {
+            if (Inbox.Count < Capacity)
+            {
+                Inbox.Add(mail);
+            }
+        }
+
+        public bool DeleteMail(string sender)
+        {
+            if (Inbox.Any(s => s.Sender == sender))
+            {
+                Inbox.RemoveAll(s => s.Sender == sender);
+                return true;
+            }
+
+            return false;
+        }
+
+        public int ArchiveInboxMessages()
+        {
+            foreach (Mail mail in Inbox)
+            {
+                Archive.Add(mail);
+            }
+
+            Inbox.Clear();
+            return Archive.Count;
+        }
+
+        public string GetLongestMessage()
+        {
+            return Inbox.OrderByDescending(b => b.Body).First().ToString();
+        }
+
+        public string InboxView()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Inbox:");
+            
+            foreach (Mail mail in Inbox)
+            {
+                sb.AppendLine($"From: {mail.Sender} / To: {mail.Receiver}");
+                sb.AppendLine($"Message: {mail.Body}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
