d8df81de5d96344ae70b97a82d32f0dd31f2fbd1
Vasil Delchev <vasildelchev97@gmail.com>
2024-02-14 22:41:57 +0200

    Added exercises

---
+++ 09.ExamPreparation/ApocalypsePreparation/Program.cs
@@ -0,0 +1,93 @@
+﻿
+int[] textilesArray = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+int[] medicamentsArray = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+Queue<int> textiles = new Queue<int>(textilesArray);
+Stack<int> medicaments = new Stack<int>(medicamentsArray);
+
+Dictionary<string, int> items = new Dictionary<string, int>
+{
+    {"Patch", 0},
+    {"Bandage", 0},
+    {"MedKit", 0 }
+};
+
+while (textiles.Any() && medicaments.Any())
+{
+    int currentComboSum = textiles.Peek() + medicaments.Peek();
+
+    if (currentComboSum == 30)
+    {
+        items["Patch"]++;
+        textiles.Dequeue();
+        medicaments.Pop();
+    }
+    else if (currentComboSum == 40)
+    {
+        items["Bandage"]++;
+        textiles.Dequeue();
+        medicaments.Pop();
+    }
+    else if (currentComboSum == 100)
+    {
+        items["MedKit"]++;
+        textiles.Dequeue();
+        medicaments.Pop();
+    }
+    else if (currentComboSum > 100)
+    {
+        items["MedKit"]++;
+
+        int remainingResource = currentComboSum - 100;
+        textiles.Dequeue();
+        medicaments.Pop();
+        medicaments.Push(medicaments.Pop() + remainingResource);
+    }
+    else
+    {
+        textiles.Dequeue();
+        medicaments.Push(medicaments.Pop() + 10);
+    }
+}
+
+if (!textiles.Any() && !medicaments.Any())
+{
+    Console.WriteLine("Textiles and medicaments are both empty.");
+}
+else if (!textiles.Any())
+{
+    Console.WriteLine("Textiles are empty.");
+}
+else if (!medicaments.Any())
+{
+    Console.WriteLine("Medicaments are empty.");
+}
+
+foreach (var item in items.OrderByDescending(amount => amount.Value).ThenBy(name => name.Key))
+{
+    if (item.Value > 0)
+    {
+        Console.WriteLine($"{item.Key} - {item.Value}");
+    }
+}
+
+if (medicaments.Any())
+{
+    Console.Write($"Medicaments left: ");
+    Console.Write(string.Join(", ", medicaments));
+    Console.WriteLine();
+}
+
+if (textiles.Any())
+{
+    Console.Write($"Textiles left: ");
+    Console.Write(string.Join(", ", textiles));
+    Console.WriteLine();
+}
---
+++ 09.ExamPreparation/BlindMan'sBuff/Program.cs
@@ -0,0 +1,102 @@
+﻿
+int[] fieldSize = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+int rows = fieldSize[0];
+int cols = fieldSize[1];
+
+char[,] field = new char[rows, cols];
+
+int currentRow = 0;
+int currentCol = 0;
+
+for (int row = 0; row < rows; row++)
+{
+    char[] fieldsymbols = Console.ReadLine()
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+        .Select(char.Parse)
+        .ToArray();
+
+    for (int col = 0; col < cols; col++)
+    {
+        field[row, col] = fieldsymbols[col];
+
+        if (field[row, col] == 'B')
+        {
+            currentRow = row;
+            currentCol = col;
+        }
+    }
+}
+
+// furniture or other obstacles will be marked with the letter 'O'
+// opponents will be marked with the letter 'P'. There will always be three other players participating in the game
+// All of the empty positions will be marked with '-'
+
+string command = string.Empty;
+
+int movesCount = 0;
+
+int touchedPlayers = 0;
+
+while ((command = Console.ReadLine()) != "Finish")
+{
+    int lastRow = currentRow;
+    int lastCol = currentCol;
+
+    if (command == "up")
+    {
+        currentRow--;
+    }
+    else if (command == "down")
+    {
+        currentRow++;
+    }
+    else if (command == "left")
+    {
+        currentCol--;
+    }
+    else if (command == "right")
+    {
+        currentCol++;
+    }
+
+    if (!isPositionVallid(currentRow, currentCol, field) || field[currentRow, currentCol] == 'O')
+    {
+        currentRow = lastRow;
+        currentCol = lastCol;
+    }
+    else
+    {
+        movesCount++;
+
+        if (field[currentRow, currentCol] == '-')
+        {
+            field[currentRow, currentCol] = 'B';
+            field[lastRow, lastCol] = '-';
+        }
+        else if (field[currentRow, currentCol] == 'P')
+        {
+            touchedPlayers++;
+
+            field[currentRow, currentCol] = 'B';
+            field[lastRow, lastCol] = '-';
+
+            if (touchedPlayers == 3)
+            {
+                break;  
+            }
+        }
+    }
+}
+
+Console.WriteLine("Game over!");
+Console.WriteLine($"Touched opponents: {touchedPlayers} Moves made: {movesCount}");
+
+bool isPositionVallid(int currentRow, int currentCol, char[,] field)
+{
+    return currentRow >= 0 && currentRow < field.GetLength(0) &&
+           currentCol >= 0 && currentCol < field.GetLength(1);
+}
